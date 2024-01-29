1997e4707eaae1c266f213d820f8db8200ad2b5b
mbenz99 <150233658+mbenz99@users.noreply.github.com>
2024-01-29 17:18:42 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // You can add your own styles here
+import UserList from './UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
